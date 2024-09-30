6ede8b027c5bfab50a1c26fa0759bd45f18601a7
Kevin Manzano <verdegriss1@gmail.com>
2024-09-29 23:03:57 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import './App.css';
 import Navbar from './assets/Componentes/Navbar';
 import Home from './assets/Componentes/Home';
 import Footer from './assets/Componentes/Footer';
+import Register from './assets/Componentes/Register';
+import Login from './assets/Componentes/Login';
+
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
-      <Home />
+      {/* <Register /> */}
+      <Login />
+      {/* <Home /> */}
       <Footer />
     </div>
   );
---
+++ src/assets/Componentes/Login.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+
+const Login = () => {
+
+    //Estados del formulario
+    const [email, setEmail] = useState('');
+    const [contraseña, setContraseña] = useState('');
+    
+    //Estado para los errores
+    const [error, setError] = useState(false);
+    
+    //Función antes de enviar el formulario
+    const validarDatos = (e) => {
+    e.preventDefault();
+    //Validación;
+    if (!email.trim() || !contraseña.trim()) {
+    setError(true);
+    return;
+    }
+
+    if (!email === '' || !email.includes('@')) {
+        alert('Campo email está vacío')
+        return;
+    }
+
+    if (contraseña.length < 6) {
+        alert('La contraseña debe tener a menos 6 caracteres')
+        return;
+    }
+
+        alert('Datos ingresados de manera correcta!')
+    
+    // Si el formulario se envía correctamente devolvemos todos nuestros estados al inicial y reseteamos el formulario
+    setError(false);
+    setEmail('');
+    setContraseña('');
+    
+    setError(false);
+    };
+    return (
+    <div className='Login'> 
+    <form className="formulario" onSubmit={validarDatos}>
+    {error ? <p className='alert' >Todos los campos son obligatorios</p> : null}
+    <div className="form-group">
+    <label>Email</label>
+    <input
+    type="email"
+    name="email"
+    className="form-control"
+    onChange={(e) => setEmail(e.target.value)}
+    value={email}
+    />
+    
+    </div>
+    <div className="form-group">
+    <label>Contraseña</label>
+    <input
+    type="password"
+    name="password"     
+    className="form-control"
+    onChange={(e) => setContraseña(e.target.value)}
+    value={contraseña}
+    />
+    </div>
+    <button type="submit" className="btn btn-primary">
+    Enviar
+    </button>
+    </form>
+    <hr />
+    
+    </div>
+    );
+    };
+    export default Login;
+    
