3ea36af16ab18661484d2b170bf365df6b8a9bb2
Kevin Manzano <verdegriss1@gmail.com>
2024-10-06 11:33:05 -0300

    login y register comentados

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import './App.css';
 import Navbar from './assets/Componentes/Navbar';
 import Home from './assets/Componentes/Home';
 import Footer from './assets/Componentes/Footer';
-import Register from './assets/Componentes/Register';
-import Login from './assets/Componentes/Login';
+// import Register from './assets/Componentes/Register';
+// import Login from './assets/Componentes/Login';
 
 // Comentar para visualizar otro contenido
 function App() {
   return (
     <div className='App'>
       <Navbar />
-      {/* <Register /> */}
-      <Login />
-      {/* <Home /> */}
+      {/* <Register />
+      <Login /> */}
+      <Home />
       <Footer />
     </div>
   );
