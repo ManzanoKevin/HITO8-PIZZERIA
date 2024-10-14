6bc51edcc016f22d39bc5073c6b808f4813e912a
Kevin Manzano <verdegriss1@gmail.com>
2024-10-14 12:37:51 -0300

    Consumo de API HOME y PIZZA

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css';
 import Navbar from './assets/Componentes/Navbar';
 // import Home from './assets/Componentes/Home';
-import Cart from './assets/Componentes/Cart';
+// import Cart from './assets/Componentes/Cart';
+import Pizza from './assets/Componentes/Pizza';
 import Footer from './assets/Componentes/Footer';
 // import Register from './assets/Componentes/Register';
 // import Login from './assets/Componentes/Login';
@@ -11,10 +12,11 @@ const App = () => {
   return (
     <div className='App'>
       <Navbar />
+      <Pizza />
       {/* <Register /> */}
       {/* <Login /> */}
       {/* <Home /> */}
-      <Cart />
+      {/* <Cart /> */}
       <Footer />
     </div>
   );
--- src/assets/Componentes/Home.jsx
+++ src/assets/Componentes/Home.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import Header from './Header'; 
 import CardPizza from './CardPizza';
-import { pizzas } from './pizzas';
 
 const Home = () => {
+
+    const [pizza, setPizza] = useState([]);
+
+    useEffect(() => {
+        const fetchPizza = async () => {
+            try {
+                const response = await fetch("http://localhost:5000/api/pizzas");
+                const data = await response.json();
+                setPizza(data);
+            } catch (error) {
+                console.error("Error fetching pizza:", error);
+            }
+        };
+        fetchPizza();
+    }, []);
+
     return (
         <div className='container-fluid'>
             <Header />
             <div className='row justify-content-center' >
-                {pizzas.map((pizza) => (
+                {pizza.map((pizza) => (
                     <div key={pizza.id} className="col-lg-4 col-md-6 col-sm-12 mb-5 d-flex">
                         <CardPizza pizza={pizza} />
                     </div>
